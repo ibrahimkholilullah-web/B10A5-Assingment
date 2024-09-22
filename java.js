250cf8990ad44c1031d95ec7481266e3a87ffa37
ibrahimkholilullah-web <nijmehalext@gmail.com>
2024-09-23 03:16:39 +0600

    projects 50% Complite

---
+++ java.js
@@ -0,0 +1,129 @@
+function getElementAllIdSelect(id){
+    const textValue = document.getElementById(id).innerText;
+    const textValues = parseFloat(textValue);
+    return textValues
+}
+function getInputById (id){
+    const inputValue = document.getElementById(id).value;
+    const inputValues = parseFloat(inputValue);
+    return inputValues;
+}
+
+document.getElementById('blog-tab').addEventListener('click', function(){
+    window.location.href = 'blog.html'
+
+})
+
+//All donate section
+
+document.getElementById('donate-btn-1').addEventListener('click', function(){
+   const noakhaliDonateMoney = getElementAllIdSelect('donate-money-1')
+   const myAcoundMoney = getElementAllIdSelect('my-id-money');
+   const noakhiteInputMoney = getInputById('noakhali-donate-inpur')
+   if(isNaN(noakhiteInputMoney) || noakhiteInputMoney < 0){
+     alert('Please Enter Balide Number')
+      return
+   }
+   if(noakhaliDonateMoney > myAcoundMoney){
+    return alert('You dont have enough money')
+   }
+    const newDonateMoney = noakhiteInputMoney + noakhaliDonateMoney;
+    document.getElementById('donate-money-1').innerText = newDonateMoney;
+    const newMyAcountMoney = myAcoundMoney - noakhiteInputMoney;
+    document.getElementById('my-id-money').innerText = newMyAcountMoney;
+   const div = document.createElement('div')
+   div.className = 'border p-4 runded-xl my-4 '
+   div.innerHTML = `
+    <p class= "md:text-xl text-sm">${noakhiteInputMoney} Taka is Donated for Donated for Flood ot Noakhali, Bangladesh</p>
+    <p class="my-3 md:text-xl text-sm bg-slate-100 rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
+   `
+   const historySection = document.getElementById('history-section');
+   historySection.appendChild(div)
+
+});
+document.getElementById('donate-btn-2').addEventListener('click', function(){
+   const feniDonateMoney = getElementAllIdSelect('feni-money')
+   const myAcoundMoney = getElementAllIdSelect('my-id-money');
+   const feniInputMoney = getInputById('feni-donate-input')
+   
+   if(isNaN(feniInputMoney) || feniInputMoney < 0){
+    alert('Please Enter Balide Number')
+     return
+  }
+  if(feniInputMoney > myAcoundMoney){
+    return alert('You dont have enough money')
+   }
+   const newDonateMoney = feniDonateMoney + feniInputMoney;
+   document.getElementById('feni-money').innerText = newDonateMoney;
+   const newMyAcountMoney = myAcoundMoney - feniInputMoney;
+   document.getElementById('my-id-money').innerText = newMyAcountMoney;
+
+   const div = document.createElement('div')
+   div.className = 'border p-4 runded-xl my-4 '
+   div.innerHTML = `
+    <p class="md:text-xl text-sm">${feniInputMoney} Donate for Flood Relief in Feni,Bangladesh</p>
+    <p class="my-3 md:text-xl text-sm bg-slate-100 rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
+   `
+   
+   const historySection = document.getElementById('history-section');
+   historySection.appendChild(div)
+});
+
+document.getElementById('donate-btn-3').addEventListener('click', function(){
+   const quoteDonateMoney = getElementAllIdSelect('quote-money')
+   const myAcoundMoney = getElementAllIdSelect('my-id-money');
+   const quoteInputMoney = getInputById('quote-donate-input')
+   
+   if(isNaN(quoteInputMoney) || quoteInputMoney < 0){
+    alert('Please Enter Balide Number')
+     return
+  }
+  if(quoteInputMoney > myAcoundMoney){
+    return alert('You dont have enough money')
+   }
+   const newDonateMoney = quoteDonateMoney + quoteInputMoney;
+   document.getElementById('quote-money').innerText = newDonateMoney;
+   const newMyAcountMoney = myAcoundMoney - quoteInputMoney;
+   document.getElementById('my-id-money').innerText = newMyAcountMoney;
+
+   const div = document.createElement('div')
+   div.className = 'border p-4  my-4 '
+   div.innerHTML = `
+    <p class= "md:text-xl text-sm">${quoteInputMoney}  Aid for Injured in the Quota Movement</p>
+    <p class="my-3 bg-slate-100 md:text-xl text-sm rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
+   `
+   
+   const historySection = document.getElementById('history');
+   historySection.insertBefore(div, historySection.firstChild)
+});
+
+// history btn Selection
+    document.getElementById('history-btn').addEventListener('click', function(){
+        document.getElementById('history-section').classList.remove('hidden')
+        document.getElementById('donation').classList.add('hidden')
+        const historyTab = document.getElementById('history-btn');
+        historyTab.classList.add(
+            "mr-4",
+            "bg-[#B4F461]"
+        )
+        const donationTba = document.getElementById('donation-btn')
+        donationTba.classList.remove(
+            "ml-4",
+            "bg-[#B4F461]"
+        )
+    });
+    //donates section
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('donation').classList.remove('hidden')
+    document.getElementById('history-section').classList.add('hidden')
+    const historyTab = document.getElementById('history-btn');
+        historyTab.classList.remove(
+            "mr-4",
+            "bg-[#B4F461]"
+        )
+        const donationTba = document.getElementById('donation-btn')
+        donationTba.classList.add(
+            "mr-4",
+            "bg-[#B4F461]"
+        )
+})
