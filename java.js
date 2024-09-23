47da8cde2a57d79e10a65bed9878c58c74ff7901
ibrahimkholilullah-web <nijmehalext@gmail.com>
2024-09-23 11:11:33 +0600

    80% complite Projects

--- java.js
+++ java.js
@@ -8,14 +8,13 @@ function getInputById (id){
     const inputValues = parseFloat(inputValue);
     return inputValues;
 }
-
+// blog btn
 document.getElementById('blog-tab').addEventListener('click', function(){
     window.location.href = 'blog.html'
 
 })
 
-//All donate section
-
+//donae for noakhile section
 document.getElementById('donate-btn-1').addEventListener('click', function(){
    const noakhaliDonateMoney = getElementAllIdSelect('donate-money-1')
    const myAcoundMoney = getElementAllIdSelect('my-id-money');
@@ -24,23 +23,26 @@ document.getElementById('donate-btn-1').addEventListener('click', function(){
      alert('Please Enter Balide Number')
       return
    }
-   if(noakhaliDonateMoney > myAcoundMoney){
+   if(noakhiteInputMoney > myAcoundMoney){
     return alert('You dont have enough money')
    }
+    
     const newDonateMoney = noakhiteInputMoney + noakhaliDonateMoney;
     document.getElementById('donate-money-1').innerText = newDonateMoney;
     const newMyAcountMoney = myAcoundMoney - noakhiteInputMoney;
     document.getElementById('my-id-money').innerText = newMyAcountMoney;
-   const div = document.createElement('div')
-   div.className = 'border p-4 runded-xl my-4 '
-   div.innerHTML = `
+    const div = document.createElement('div')
+    div.className = 'border p-4 rounded-lg my-4 '
+    div.innerHTML = `
     <p class= "md:text-xl text-sm">${noakhiteInputMoney} Taka is Donated for Donated for Flood ot Noakhali, Bangladesh</p>
     <p class="my-3 md:text-xl text-sm bg-slate-100 rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
-   `
-   const historySection = document.getElementById('history-section');
-   historySection.appendChild(div)
-
+    `
+    const historySection = document.getElementById('history-section');
+    historySection.appendChild(div)
+    document.getElementById('my_modal_1').showModal();
 });
+
+//donate for feni section
 document.getElementById('donate-btn-2').addEventListener('click', function(){
    const feniDonateMoney = getElementAllIdSelect('feni-money')
    const myAcoundMoney = getElementAllIdSelect('my-id-money');
@@ -49,26 +51,26 @@ document.getElementById('donate-btn-2').addEventListener('click', function(){
    if(isNaN(feniInputMoney) || feniInputMoney < 0){
     alert('Please Enter Balide Number')
      return
-  }
-  if(feniInputMoney > myAcoundMoney){
+   }
+   if(feniInputMoney > myAcoundMoney){
     return alert('You dont have enough money')
    }
    const newDonateMoney = feniDonateMoney + feniInputMoney;
    document.getElementById('feni-money').innerText = newDonateMoney;
    const newMyAcountMoney = myAcoundMoney - feniInputMoney;
    document.getElementById('my-id-money').innerText = newMyAcountMoney;
-
    const div = document.createElement('div')
-   div.className = 'border p-4 runded-xl my-4 '
+   div.className = 'border p-4 rounded-lg my-4 '
    div.innerHTML = `
     <p class="md:text-xl text-sm">${feniInputMoney} Donate for Flood Relief in Feni,Bangladesh</p>
     <p class="my-3 md:text-xl text-sm bg-slate-100 rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
    `
-   
    const historySection = document.getElementById('history-section');
-   historySection.appendChild(div)
+   historySection.appendChild(div);
+   document.getElementById('my_modal_2').showModal();
 });
 
+// quote Movement section
 document.getElementById('donate-btn-3').addEventListener('click', function(){
    const quoteDonateMoney = getElementAllIdSelect('quote-money')
    const myAcoundMoney = getElementAllIdSelect('my-id-money');
@@ -77,8 +79,8 @@ document.getElementById('donate-btn-3').addEventListener('click', function(){
    if(isNaN(quoteInputMoney) || quoteInputMoney < 0){
     alert('Please Enter Balide Number')
      return
-  }
-  if(quoteInputMoney > myAcoundMoney){
+   }
+   if(quoteInputMoney > myAcoundMoney){
     return alert('You dont have enough money')
    }
    const newDonateMoney = quoteDonateMoney + quoteInputMoney;
@@ -87,43 +89,45 @@ document.getElementById('donate-btn-3').addEventListener('click', function(){
    document.getElementById('my-id-money').innerText = newMyAcountMoney;
 
    const div = document.createElement('div')
-   div.className = 'border p-4  my-4 '
+   div.className = 'border p-4 rounded-lg my-4 '
    div.innerHTML = `
-    <p class= "md:text-xl text-sm">${quoteInputMoney}  Aid for Injured in the Quota Movement</p>
-    <p class="my-3 bg-slate-100 md:text-xl text-sm rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
+   <p class= "md:text-xl text-sm">${quoteInputMoney}  Aid for Injured in the Quota Movement</p>
+   <p class="my-3 bg-slate-100 md:text-xl text-sm rounded-xl p-2">Date : ${new Date().toLocaleDateString()} (Bangladesh Standard Time)</p>
    `
-   
    const historySection = document.getElementById('history');
-   historySection.insertBefore(div, historySection.firstChild)
+   historySection.insertBefore(div, historySection.firstChild);
+   document.getElementById('my_modal_3').showModal();
+
 });
 
 // history btn Selection
-    document.getElementById('history-btn').addEventListener('click', function(){
-        document.getElementById('history-section').classList.remove('hidden')
-        document.getElementById('donation').classList.add('hidden')
-        const historyTab = document.getElementById('history-btn');
-        historyTab.classList.add(
-            "mr-4",
-            "bg-[#B4F461]"
-        )
-        const donationTba = document.getElementById('donation-btn')
-        donationTba.classList.remove(
-            "ml-4",
-            "bg-[#B4F461]"
-        )
-    });
-    //donates section
+document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('history-section').classList.remove('hidden')
+    document.getElementById('donation').classList.add('hidden')
+    const historyTab = document.getElementById('history-btn');
+    historyTab.classList.add(
+        "mr-4",
+        "bg-[#B4F461]"
+    )
+    const donationTba = document.getElementById('donation-btn')
+    donationTba.classList.remove(
+        "ml-4",
+        "bg-[#B4F461]"
+    )
+});
+
+//donates section
 document.getElementById('donation-btn').addEventListener('click', function(){
     document.getElementById('donation').classList.remove('hidden')
     document.getElementById('history-section').classList.add('hidden')
     const historyTab = document.getElementById('history-btn');
-        historyTab.classList.remove(
-            "mr-4",
-            "bg-[#B4F461]"
-        )
-        const donationTba = document.getElementById('donation-btn')
-        donationTba.classList.add(
-            "mr-4",
-            "bg-[#B4F461]"
-        )
+    historyTab.classList.remove(
+        "mr-4",
+        "bg-[#B4F461]"
+    )
+    const donationTba = document.getElementById('donation-btn')
+    donationTba.classList.add(
+        "mr-4",
+        "bg-[#B4F461]"
+    )
 })
